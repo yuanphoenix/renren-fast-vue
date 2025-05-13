88dfe54cfe66665723f287cac0a149131b3bbbec
tifa <tifa@139.com>
2025-05-13 23:37:17 +0800

    完成刪除節點操作

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,6 +1,19 @@
 <template>
     <div>
-        <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+        <el-tree :data="menus" :props="defaultProps" show-checkbox node-key="catId" default-expand-all
+            :expand-on-click-node="false">
+            <span class="custom-tree-node" slot-scope="{ node, data }">
+                <span>{{ node.label }}</span>
+                <span>
+                    <el-button type="text" size="mini" @click="() => append(data)">
+                        添加
+                    </el-button>
+                    <el-button v-if="!data.children.length" type="text" size="mini" @click="() => remove(node, data)">
+                        删除
+                    </el-button>
+                </span>
+            </span>
+        </el-tree>
     </div>
 </template>
 
@@ -26,12 +39,37 @@ export default {
             console.log(data);
         },
 
+        append(data) {
+           
+        },
+
+        remove(node, data) {
+            this.$http({
+                url: this.$http.adornUrl('/product/category/delete','gateway'),
+                method: 'post',
+                data: this.$http.adornData(data, false)
+            }).then(({ data }) => {
+                if (data && data.code === 200) {
+                    this.$message({
+                        message: '操作成功',
+                        type: 'success',
+                        duration: 1500,
+                        onClose: () => {
+                            this.getMenus()
+                        }
+                    })
+                } else {
+                    this.$message.error(data.msg)
+                }
+            })
+        },
+
         getMenus() {
             this.$http({
                 url: this.$http.adornUrl('/product/category/list/tree', "gateway"),
                 method: 'get',
             }).then(({ data }) => {
-                this.menus=data.data
+                this.menus = data.data
             })
         }
     }
