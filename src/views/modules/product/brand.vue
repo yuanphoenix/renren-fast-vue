19e62bdbcd1d902ccb8d2ac5ef55606d4d1b6838
tifa <tifa@139.com>
2025-05-23 23:54:54 +0800

    品牌与类别关联

--- src/views/modules/product/brand.vue
+++ src/views/modules/product/brand.vue
@@ -43,7 +43,7 @@
       </el-table-column>
       <el-table-column fixed="right" header-align="center" align="center" width="150" label="操作">
         <template slot-scope="scope">
-
+          <el-button type="text" size="small" @click="updateCatalogHandle(scope.row.brandId)">关联分类</el-button>
           <el-button type="text" size="small" @click="addOrUpdateHandle(scope.row.brandId)">修改</el-button>
           <el-button type="text" size="small" @click="deleteHandle(scope.row.brandId)">删除</el-button>
         </template>
@@ -55,11 +55,43 @@
     </el-pagination>
     <!-- 弹窗, 新增 / 修改 -->
     <add-or-update v-if="addOrUpdateVisible" ref="addOrUpdate" @refreshDataList="getDataList"></add-or-update>
+
+
+    <el-dialog title="关联分类" :visible.sync="cateRelationDialogVisible" width="30%">
+      <el-popover placement="right-end" v-model="popCatalogSelectVisible">
+        <category-cascader :catalogPath.sync="catalogPath"></category-cascader>
+        <div style="text-align: right; margin: 0">
+          <el-button size="mini" type="text" @click="popCatalogSelectVisible = false">取消</el-button>
+          <el-button type="primary" size="mini" @click="addCatalogSelect">确定</el-button>
+        </div>
+        <el-button slot="reference">新增关联</el-button>
+      </el-popover>
+      <el-table :data="cateRelationTableData" style="width: 100%">
+        <el-table-column prop="id" label="ID"></el-table-column>
+        <el-table-column prop="brandName" label="品牌名"></el-table-column>
+        <el-table-column prop="catalogName" label="分类名"></el-table-column>
+        <el-table-column fixed="right" header-align="center" align="center" label="操作">
+          <template slot-scope="scope">
+            <el-button
+              type="text"
+              size="small"
+              @click="deleteCateRelationHandle(scope.row.id,scope.row.brandId)"
+            >移除
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="cateRelationDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="cateRelationDialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 import AddOrUpdate from './brand-add-or-update'
+import CategoryCascader from '../common/category-cascader'
 
 export default {
   data () {
@@ -67,30 +99,73 @@ export default {
       dataForm: {
         key: ''
       },
+      brandId: 0,
+      catalogPath: [],
       dataList: [],
+      cateRelationTableData: [],
       pageIndex: 1,
       pageSize: 10,
       totalPage: 0,
       dataListLoading: false,
       dataListSelections: [],
-      addOrUpdateVisible: false
+      addOrUpdateVisible: false,
+      cateRelationDialogVisible: false,
+      popCatalogSelectVisible: false
     }
   },
   components: {
-    AddOrUpdate
+    AddOrUpdate,
+    CategoryCascader
   },
   activated () {
     this.getDataList()
   },
   methods: {
-    // 获取数据列表
-
-    updateStatus (data) {
-
-      let {brandId, showStatus} = data
-
+    addCatalogSelect () {
+      //{"brandId":1,"catalogId":2}
+      this.popCatalogSelectVisible = false
       this.$http({
-        url: this.$http.adornUrl('/product/brand/update', 'gateway'),
+        url: this.$http.adornUrl('/product/categorybrandrelation/save', 'gateway'),
+        method: 'post',
+        data: this.$http.adornData({
+          brandId: this.brandId,
+          catalogId: this.catalogPath[this.catalogPath.length - 1]
+        }, false)
+      }).then(({data}) => {
+        this.getCateRelation()
+      })
+    },
+    deleteCateRelationHandle (id, brandId) {
+      this.$http({
+        url: this.$http.adornUrl(`/product/categorybrandrelation/delete/${id}`, 'gateway'),
+        method: 'post'
+      }).then(({data}) => {
+        this.getCateRelation()
+      })
+    },
+    updateCatalogHandle (brandId) {
+      // 这是关联首先进入的函数
+      this.cateRelationDialogVisible = true
+      this.brandId = brandId
+      this.getCateRelation()
+    },
+    getCateRelation () {
+      // 这是关联第二步进入的函数
+      this.$http({
+        url: this.$http.adornUrl('/product/categorybrandrelation/catalog/list', 'gateway'),
+        method: 'get',
+        params: this.$http.adornParams({
+          brandId: this.brandId
+        })
+      }).then(({data}) => {
+        this.cateRelationTableData = data.data
+      })
+    },
+    // 获取数据列表
+    updateStatus (data) {
+      let {brandId, showStatus} = data
+      this.$http({
+        url: this.$http.adornUrl('/product/brand/update', 'gateway', 'gateway'),
         method: 'post',
         data: this.$http.adornData({brandId, showStatus}, false)
       }).then(({data}) => {
@@ -99,7 +174,6 @@ export default {
         } else {
           this.$message.error(data.msg)
         }
-
       })
 
     },
--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,10 +1,9 @@
 <template>
-    <div>
-
-        <el-switch v-model="draggable" active-text="开启拖拽" inactive-text="关闭拖拽"></el-switch>
-        <el-button type="danger" @click="batch_delete()" round>批量删除</el-button>
-        <el-tree :data="menus" :props="defaultProps" show-checkbox node-key="catId" default-expand-all ref="tree"
-            :draggable="draggable" :expand-on-click-node="false" :allow-drop="allowDrop" @node-drop="handleDrop">
+  <div>
+    <el-switch v-model="draggable" active-text="开启拖拽" inactive-text="关闭拖拽"></el-switch>
+    <el-button type="danger" @click="batch_delete()" round>批量删除</el-button>
+    <el-tree :data="menus" :props="defaultProps" show-checkbox node-key="catId" default-expand-all ref="tree"
+             :draggable="draggable" :expand-on-click-node="false" :allow-drop="allowDrop" @node-drop="handleDrop">
             <span class="custom-tree-node" slot-scope="{ node, data }">
                 <span>{{ node.label }}</span>
                 <span>
@@ -15,167 +14,170 @@
                         修改
                     </el-button>
                     <el-button v-if="data.leaf" type="text" size="mini"
-                        @click="() => remove(node, data)">
+                               @click="() => remove(node, data)">
                         删除
                     </el-button>
                 </span>
             </span>
-        </el-tree>
-    </div>
+    </el-tree>
+  </div>
 </template>
 
 <script>
 
 export default {
-    data() {
-        return {
-            draggable: false,
-            menus: [],
-            defaultProps: {
-                children: 'children',
-                label: 'name'
-            }
-        };
-    },
-    created() {
-        this.getMenus();
-    },
-    methods: {
-        handleNodeClick(data) {
-            console.log(data);
-        },
-
-        batch_delete() {
-            this.$http({
-                url: this.$http.adornUrl('/product/category/batchDelete', 'gateway'),
-                method: 'post',
-                data: this.$http.adornData(this.$refs.tree.getCheckedNodes(), false)
-            }).then(({ data }) => {
-                if (data && data.code === 200) {
-                    this.$message({
-                        message: '操作成功',
-                        type: 'success',
-                        duration: 1500,
-                        onClose: () => {
-                            this.getMenus()
-                        }
-                    })
-                } else {
-                    this.$message.error(data.msg)
-                }
-            })
-
-
-        },
-
-        edit(data) {
-            const labelName = prompt('请输入节点名称:', '');
-            if (labelName === null || !labelName) return; // 用户点击了取消
-            data.name = labelName
-
-            this.$http({
-                url: this.$http.adornUrl('/product/category/update', 'gateway'),
-                method: 'post',
-                data: this.$http.adornData(data, false)
-            }).then(({ data }) => {
-                if (data && data.code === 200) {
-                    this.$message({
-                        message: '操作成功',
-                        type: 'success',
-                        duration: 1500,
-                        onClose: () => {
-                            this.getMenus()
-                        }
-                    })
-                } else {
-                    this.$message.error(data.msg)
-                }
-            })
-        },
-
-        append(data) {
-            const labelName = prompt('请输入节点名称:', '');
-            if (labelName === null || !labelName) return; // 用户点击了取消
-
-            const newChild = {
-                name: labelName || '未命名节点',  // 处理空输入
-                parentCid: data.catId
-            };
-
-            this.$http({
-                url: this.$http.adornUrl('/product/category/save', 'gateway'),
-                method: 'post',
-                data: this.$http.adornData(newChild, false)
-            }).then(({ data }) => {
-                if (data && data.code === 200) {
-                    this.$message({
-                        message: '操作成功',
-                        type: 'success',
-                        duration: 1500,
-                        onClose: () => {
-                            this.getMenus()
-                        }
-                    })
-                } else {
-                    this.$message.error(data.msg)
-                }
-            })
-        },
-
-        handleDrop(draggingNode, dropNode, dropType, ev) {
-            this.$http({
-                url: this.$http.adornUrl('/product/category/sort', 'gateway'),
-                method: 'post',
-                data: this.$http.adornData({ draggingNodeId: draggingNode.data.catId, dropNodeId: dropNode.data.catId, dropType: dropType }, false)
-            }).then(({ data }) => {
-                if (data && data.code === 200) {
-                    this.$message({
-                        message: '菜单拖拽成功',
-                        type: 'success',
-                        duration: 1500,
-                        onClose: () => {
-                            this.getMenus()
-                        }
-                    })
-                } else {
-                    this.$message.error(data.msg)
-                }
-            })
-        },
-
-        allowDrop(draggingNode, dropNode, type) {
-            return true
-        },
-        remove(node, data) {
-            this.$http({
-                url: this.$http.adornUrl('/product/category/delete', 'gateway'),
-                method: 'post',
-                data: this.$http.adornData(data, false)
-            }).then(({ data }) => {
-                if (data && data.code === 200) {
-                    this.$message({
-                        message: '操作成功',
-                        type: 'success',
-                        duration: 1500,
-                        onClose: () => {
-                            this.getMenus()
-                        }
-                    })
-                } else {
-                    this.$message.error(data.msg)
-                }
-            })
-        },
-
-        getMenus() {
-            this.$http({
-                url: this.$http.adornUrl('/product/category/list/tree', "gateway"),
-                method: 'get',
-            }).then(({ data }) => {
-                this.menus = data.data
-            })
-        }
+  data () {
+    return {
+      draggable: false,
+      menus: [],
+      defaultProps: {
+        children: 'children',
+        label: 'name'
+      }
     }
+  },
+  created () {
+    this.getMenus()
+  },
+  methods: {
+    handleNodeClick (data) {
+      console.log(data)
+    },
+
+    batch_delete () {
+      this.$http({
+        url: this.$http.adornUrl('/product/category/batchDelete', 'gateway'),
+        method: 'post',
+        data: this.$http.adornData(this.$refs.tree.getCheckedNodes(), false)
+      }).then(({data}) => {
+        if (data && data.code === 200) {
+          this.$message({
+            message: '操作成功',
+            type: 'success',
+            duration: 1500,
+            onClose: () => {
+              this.getMenus()
+            }
+          })
+        } else {
+          this.$message.error(data.msg)
+        }
+      })
+
+    },
+
+    edit (data) {
+      const labelName = prompt('请输入节点名称:', '')
+      if (labelName === null || !labelName) return // 用户点击了取消
+      data.name = labelName
+
+      this.$http({
+        url: this.$http.adornUrl('/product/category/update', 'gateway'),
+        method: 'post',
+        data: this.$http.adornData(data, false)
+      }).then(({data}) => {
+        if (data && data.code === 200) {
+          this.$message({
+            message: '操作成功',
+            type: 'success',
+            duration: 1500,
+            onClose: () => {
+              this.getMenus()
+            }
+          })
+        } else {
+          this.$message.error(data.msg)
+        }
+      })
+    },
+
+    append (data) {
+      const labelName = prompt('请输入节点名称:', '')
+      if (labelName === null || !labelName) return // 用户点击了取消
+
+      const newChild = {
+        name: labelName || '未命名节点',  // 处理空输入
+        parentCid: data.catId
+      }
+
+      this.$http({
+        url: this.$http.adornUrl('/product/category/save', 'gateway'),
+        method: 'post',
+        data: this.$http.adornData(newChild, false)
+      }).then(({data}) => {
+        if (data && data.code === 200) {
+          this.$message({
+            message: '操作成功',
+            type: 'success',
+            duration: 1500,
+            onClose: () => {
+              this.getMenus()
+            }
+          })
+        } else {
+          this.$message.error(data.msg)
+        }
+      })
+    },
+
+    handleDrop (draggingNode, dropNode, dropType, ev) {
+      this.$http({
+        url: this.$http.adornUrl('/product/category/sort', 'gateway'),
+        method: 'post',
+        data: this.$http.adornData({
+          draggingNodeId: draggingNode.data.catId,
+          dropNodeId: dropNode.data.catId,
+          dropType: dropType
+        }, false)
+      }).then(({data}) => {
+        if (data && data.code === 200) {
+          this.$message({
+            message: '菜单拖拽成功',
+            type: 'success',
+            duration: 1500,
+            onClose: () => {
+              this.getMenus()
+            }
+          })
+        } else {
+          this.$message.error(data.msg)
+        }
+      })
+    },
+
+    allowDrop (draggingNode, dropNode, type) {
+      return true
+    },
+    remove (node, data) {
+      this.$http({
+        url: this.$http.adornUrl('/product/category/delete', 'gateway'),
+        method: 'post',
+        data: this.$http.adornData(data, false)
+      }).then(({data}) => {
+        if (data && data.code === 200) {
+          this.$message({
+            message: '操作成功',
+            type: 'success',
+            duration: 1500,
+            onClose: () => {
+              this.getMenus()
+            }
+          })
+        } else {
+          this.$message.error(data.msg)
+        }
+      })
+    },
+
+    getMenus () {
+      this.$http({
+        url: this.$http.adornUrl('/product/category/list/tree', 'gateway'),
+        method: 'get',
+      }).then(({data}) => {
+        this.menus = data.data
+      })
+    }
+  }
 }
 
 </script>
